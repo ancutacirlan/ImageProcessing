adcf3fccd700f57ac81bf34c29a67566ffcea09f
Cirlan Ancuta <cirlan.ancutaa@gmail.com>
2021-01-19 11:38:47 +0200

    starting a eureka server, creating eureka clients

--- image-formater/src/main/java/io/licence/imageformater/ImageFormaterApplication.java
+++ image-formater/src/main/java/io/licence/imageformater/ImageFormaterApplication.java
@@ -1,9 +1,16 @@
 package io.licence.imageformater;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class ImageFormaterApplication {
 
 	public static void main(String[] args) {
--- web-app/src/main/java/io/licence/webapp/WebAppApplication.java
+++ web-app/src/main/java/io/licence/webapp/WebAppApplication.java
@@ -2,10 +2,16 @@ package io.licence.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableEurekaClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class WebAppApplication {
 
 	public static void main(String[] args) {
